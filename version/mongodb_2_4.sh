3c8d33726f3b4549404d14e478f61d76c4ecada3
Khoa Nguyen <vankhoa011@gmail.com>
2015-10-16 23:54:31 +0700

    update script install 2_4 mongodb

--- version/mongodb_2_4.sh
+++ version/mongodb_2_4.sh
@@ -4,7 +4,7 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 sudo echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
-sudo apt-get install mongodb-10gen=2.4.13
+sudo apt-get install -y mongodb-10gen=2.4.13
 
 # Create the data directory
 sudo mkdir -p /data/db
