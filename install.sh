764026516295bcf323e5d6e0ddfa929dfd2baf27
Chris Kinsman <chris@kinsman.net>
2015-12-08 12:16:41 -0800

    Add our prereqs

--- install.sh
+++ install.sh
@@ -8,17 +8,22 @@
 # # add source of nvm to .bashrc - allows user to use nvm as a command
 # echo "source ~/.nvm/nvm.sh" >> .bashrc
 
-
-echo "================= Updating global nodejs packages ==================="
-sudo npm install -g grunt-cli mocha vows phantomjs casperjs;
-
-echo "=============== Installing bower globally ============="
-sudo npm install -g bower
-
 for file in /tmp/version/*;
 do
   $file
 done
 
+echo "================= Updating global nodejs packages ==================="
+sudo npm install -g grunt-cli mocha vows phantomjs casperjs gulp;
+
+echo "=============== Installing bower globally ============="
+sudo npm install -g bower
+
+nvm alias default 4.2.2
+
 # # tell nvm to use this version as default
 # . /root/.nvm/nvm.sh && nvm alias default 0.12
+
+virtualenv ve && source ve/bin/activate && pip install awscli
+apt-get update
+apt-get install -y -q libpq-dev
