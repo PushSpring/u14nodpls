6e491895a7fdfa90926adb7ce708e17d684d7af2
Chris Kinsman <chris@kinsman.net>
2016-06-23 09:21:06 -0700

    Move where we install globals

--- install.sh
+++ install.sh
@@ -8,23 +8,17 @@
 # # add source of nvm to .bashrc - allows user to use nvm as a command
 # echo "source ~/.nvm/nvm.sh" >> .bashrc
 
-echo "================= Updating global nodejs packages ==================="
-sudo npm install -g grunt-cli mocha vows phantomjs casperjs gulp;
-
-echo "=============== Installing bower globally ============="
-sudo npm install -g bower
 
 for file in /u14nodpls/version/*;
 do
   $file
 done
 
-echo "=============== Update npm ============="
-sudo npm install npm@2.15.1 -g
-
 virtualenv ve && source ve/bin/activate && pip install awscli
 sudo apt-get update -y
 sudo apt-get install -y -q libpq-dev
 
 # Use 4.2.2 as default
-. /root/.nvm/nvm.sh && nvm alias default 4.2.2
+. /root/.nvm/nvm.sh && nvm alias default 4.2.2 && nvm use 4.2.2
+
+
--- version/4_2_2.sh
+++ version/4_2_2.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -e
 
 echo "=============== Installing Node v. 4.0.x ============="
-. /root/.nvm/nvm.sh && nvm install 4.2.2
+. /root/.nvm/nvm.sh && nvm install 4.2.2 && nvm use 4.2.2
+
+echo "================= Updating global nodejs packages ==================="
+sudo npm install -g grunt-cli mocha vows phantomjs casperjs gulp;
+
+echo "=============== Installing bower globally ============="
+sudo npm install -g bower
+
+echo "=============== Update npm ============="
+sudo npm install npm@2.15.1 -g
